f86d43e2fa69a42014e09e5d905cbf3e01d3b16b
DavidAlejandroHenaoJaramillo <davida.henaoj@uqvirtual.edu.co>
2025-11-06 12:05:51 -0500

    Proyecto guardado

--- src/main/java/com/example/proyectofinal/App/HelloApplication.java
+++ src/main/java/com/example/proyectofinal/App/HelloApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/example/proyectofinal/View/Login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 900, 500);
+        stage.setTitle("Proyecto Final");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/proyectofinal/Controllers/LoginController.java
+++ src/main/java/com/example/proyectofinal/Controllers/LoginController.java
@@ -1,14 +1,109 @@
 package com.example.proyectofinal.Controllers;
 
+import com.example.proyectofinal.Models.User;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
+import java.util.ArrayList;
 
 public class LoginController {
     @FXML
-    private Label welcomeText;
+    private TextField txtUsername;
+    @FXML
+    private TextField txtPassword;
+    @FXML
+    private Button btnLogin;
+    @FXML
+    private Button btnCancel;
+
+    private ArrayList<User> userList = new ArrayList<>();
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void initialize() {
+        userList.add(new User("alejandro2007", "123456"));
+        userList.add(new User("Dandro07", "FliaHenaoJ11"));
+
+        btnLogin.setOnAction(e -> login());
+        btnCancel.setOnAction(e -> cancel());
+    }
+
+    @FXML
+    private void login() {
+        String username = txtUsername.getText();
+        String password = txtPassword.getText();
+
+
+        if (username.isEmpty() || password.isEmpty()) {
+            showAlertWarning("Error" , "Complete all fields");
+            return;
+        }
+        boolean found = false;
+
+        for (User user : userList) {
+            if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            showAlert("Login Succesful", "Welcome " + username);
+        } else {
+            showAlertError("Error", "Username or password incorrect / User not created");
+        }
+    }
+
+    @FXML
+    private void cancel() {
+        txtUsername.clear();
+        txtPassword.clear();
+    }
+
+    private void showAlert(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+
+    }
+    private void showAlertWarning(String title, String message) {
+        Alert alertWarning = new Alert(Alert.AlertType.WARNING);
+        alertWarning.setTitle(title);
+        alertWarning.setHeaderText(null);
+        alertWarning.setContentText(message);
+        alertWarning.showAndWait();
+    }
+
+    private void showAlertError(String title, String message) {
+        Alert alertError = new Alert(Alert.AlertType.ERROR);
+        alertError.setTitle(title);
+        alertError.setHeaderText(null);
+        alertError.setContentText(message);
+        alertError.showAndWait();
+    }
+
+
+
+    @FXML
+    protected void eventKey(KeyEvent event) {
+
+        Object evt = event.getSource();
+
+        if (evt.equals(txtUsername)) {
+
+            if (event.getCharacter().equals(" ")) {
+                event.consume();
+            }
+
+        } else if (evt.equals(txtPassword)) {
+            if (event.getCharacter().equals(" ")) {
+                event.consume();
+            }
+        }
     }
 }
