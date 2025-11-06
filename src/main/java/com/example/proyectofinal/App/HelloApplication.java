30662e3a10bb281f6ba274bbed2b9db669c91dbb
DavidAlejandroHenaoJaramillo <davida.henaoj@uqvirtual.edu.co>
2025-11-06 15:48:49 -0500

    Proyecto guardado

--- src/main/java/com/example/proyectofinal/App/HelloApplication.java
+++ src/main/java/com/example/proyectofinal/App/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/example/proyectofinal/View/Login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 900, 500);
+        Scene scene = new Scene(fxmlLoader.load(), 694, 500);
         stage.setTitle("Proyecto Final");
         stage.setScene(scene);
         stage.show();
